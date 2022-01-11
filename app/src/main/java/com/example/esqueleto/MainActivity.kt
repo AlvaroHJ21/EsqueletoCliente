ad6b83b1b3bb90b35e1d196a1fb2159f4310a23f
AlvaroHJ21 <alvarohuaysara@gmail.com>
2022-01-11 12:12:05 -0500

    ahhhh

--- app/src/main/java/com/example/esqueleto/MainActivity.kt
+++ app/src/main/java/com/example/esqueleto/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Toast.makeText(this, "FFFF", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "Ahhhhhhh", Toast.LENGTH_SHORT).show()
     }
 }
